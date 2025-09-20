aba309893133ff982d6a1f2b8324e9f2337b7d58
quyntg94 <quyntg94@gmail.com>
2025-09-20 19:14:11 +0700

    hotfix

--- js/script.js
+++ js/script.js
@@ -12,14 +12,14 @@ let timerStartAt = null;
 
 function loadPage(url) {
 	const app = document.getElementById('app');
-	fetch(url)
-	.then(res => res.text())
-	.then(html => {
-		app.innerHTML = html;
-	})
-	.catch(() => {
-		app.innerHTML = "<h2>Page not found</h2>";
-	});
+	return fetch(url)
+		.then(res => res.text())
+		.then(html => {
+			app.innerHTML = html;
+		})
+		.catch(() => {
+			app.innerHTML = "<h2>Page not found</h2>";
+		});
 }
 
 function startExam() {
